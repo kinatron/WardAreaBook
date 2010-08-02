ff29d61c5400a66af3b5f404b781da7cee20aa17
kinatronLT <kinateder@gmail.com>
2010-08-02 08:35:57 -0700

    Print capabilities for the hope report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,7 @@ class ReportsController < ApplicationController
   before_filter :store_return_point
   caches_action :hope, :month
 
+  # TODO reference the hopes by name not id 1
   def hope
     @events = Event.find_all_by_person_id(1, :order => 'date DESC')
     @event_weeks = @events.group_by { |week| week.date.at_beginning_of_week }
@@ -11,6 +12,13 @@ class ReportsController < ApplicationController
     @events = Event.find(:all, :conditions => "category == 'Visit' or category == 'Lesson'", 
                          :order => 'date DESC')
     @event_months = @events.group_by { |month| month.date.at_beginning_of_month }
+  end
+  def print
+    # TODO have the specific week sent to me
+    @week = Date.parse(params[:date])
+    @events = Event.find_all_by_person_id(1, :conditions => {:date => @week..@week.advance(:days => 6)}, 
+                                             :order => 'date DESC')
+    render :layout => 'print'
   end
 
 end
