4b36744b40b99d8f5b511d4bd8bca45621e985f9
kinatron <kinateder@gmail.com>
2010-10-05 16:56:09 -0700

    Adding colors coding to see how recent a home teaching visit was.

--- app/helpers/teaching_routes_helper.rb
+++ app/helpers/teaching_routes_helper.rb
@@ -24,12 +24,35 @@ module TeachingRoutesHelper
   end
 
   def getLastHomeTeacherVisit(family)
+    # some routes only have one home teacher.  Referencing the routes by the 
+    # array index does make this kind of brittle.  TODO
+    begin
+      hometeacher1 = family.teaching_routes[0].person.id 
+      hometeacher2 = family.teaching_routes[1].person.id 
+    rescue 
+      hometeacher1 ||=  0
+      hometeacher2 ||=  0
+    end
+
     family.events.find(:first, 
                        :conditions => ["(person_id = ? or person_id = ?) and 
                                         (category = 'Visit' or category = 'Lesson')",
-                                       family.teaching_routes[0].person.id,
-                                       family.teaching_routes[1].person.id ],
+                                        hometeacher1, hometeacher2],
                        :order => 'date DESC' )
+  rescue
+    nil
+  end
+
+  def getLastHomeTeacherVisitColor(event)
+    unless event == nil
+      if event.date > Date.today.at_beginning_of_month
+        return "#00ff00"
+      elsif event.date > Date.today.months_ago(1).at_beginning_of_month
+        return "cyan"
+      elsif event.date > Date.today.months_ago(2).at_beginning_of_month 
+        return "yellow"
+      end
+    end
   end
 
 end
