20687bc26a8b131323646aa2f773b21a25f6d3fa
Ubuntu 10.10 <ryan@ubuntu.(none)>
2011-01-03 15:47:44 -0800

    Fixed a counting bug with the stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,13 +1,13 @@
 class StatsController < ApplicationController
   caches_action :index, :layout => false
   def index 
-    @totalMembers  = Person.find_all_by_current(true).count
-    @total         = Family.find_all_by_current_and_member(true,true, :conditions => "status != 'moved'").count
-    @active        = Family.find_all_by_current_and_member_and_status(true,true,'active').count
-    @lessActive    = Family.find_all_by_current_and_member_and_status(true,true,'less active').count
-    @notInterested = Family.find_all_by_current_and_member_and_status(true,true,'not interested').count
-    @dnc           = Family.find_all_by_current_and_member_and_status(true,true,'dnc').count
-    @unknown       = Family.find_all_by_current_and_member_and_status(true,true,'unknown').count + 
-                     Family.find_all_by_current_and_member_and_status(true,true,'new').count 
+    @totalMembers  = Person.find_all_by_current(true).length
+    @total         = Family.find_all_by_current_and_member(true,true, :conditions => "status != 'moved'").length
+    @active        = Family.find_all_by_current_and_member_and_status(true,true,'active').length
+    @lessActive    = Family.find_all_by_current_and_member_and_status(true,true,'less active').length
+    @notInterested = Family.find_all_by_current_and_member_and_status(true,true,'not interested').length
+    @dnc           = Family.find_all_by_current_and_member_and_status(true,true,'dnc').length
+    @unknown       = Family.find_all_by_current_and_member_and_status(true,true,'unknown').length + 
+                     Family.find_all_by_current_and_member_and_status(true,true,'new').length 
   end
 end
