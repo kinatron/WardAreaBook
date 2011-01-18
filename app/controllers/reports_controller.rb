c5e0d0459a0a54340ec75aad76eedc395e040b96
kinatronLT <kinateder@gmail.com>
2011-01-18 11:51:39 -0800

    cleaned up some of the views in reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_filter :store_return_point
-  caches_action :hope, :month, :layout => false
+  caches_action :hope, :monthlyReport, :allMonthlyReports, :layout => false
 
   # override the application accessLevel method
   def checkAccess
@@ -18,10 +18,19 @@ class ReportsController < ApplicationController
     @event_weeks = @events.group_by { |week| week.date.at_beginning_of_week }
   end
 
-  def month
+  def print
+    # TODO have the specific week sent to me
+    @week = Date.parse(params[:date])
+    @events = Event.find_all_by_person_id(1, :conditions => {:date => @week..@week.advance(:days => 6)}, 
+                                             :order => 'date DESC')
+    render :layout => 'print'
+  end
+
+  def allMonthlyReports
     @events = Event.find(:all, :conditions => "category == 'Visit' or category == 'Lesson'", 
                          :order => 'date DESC')
     @event_months = @events.group_by { |month| month.date.at_beginning_of_month }
+    render :monthlyReport
   end
   
   def monthlyReport
@@ -31,14 +40,4 @@ class ReportsController < ApplicationController
                          :order => 'date DESC')
     @event_months = @events.group_by { |month| month.date.at_beginning_of_month }
   end
-
-
-  def print
-    # TODO have the specific week sent to me
-    @week = Date.parse(params[:date])
-    @events = Event.find_all_by_person_id(1, :conditions => {:date => @week..@week.advance(:days => 6)}, 
-                                             :order => 'date DESC')
-    render :layout => 'print'
-  end
-
 end
