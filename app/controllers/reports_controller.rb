b53fe2f9588a9e71c7c1513e17ee4586a870cb5e
Ubuntu 10.10 <ryan@ubuntu.(none)>
2011-02-27 11:26:27 -0800

    Updated the print options for the hope weekly report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -18,25 +18,6 @@ class ReportsController < ApplicationController
     @event_weeks = @events.group_by { |week| week.date.at_beginning_of_week }
   end
 
-  def print
-    # TODO have the specific week sent to me
-    @week = Date.parse(params[:date])
-    @events = Event.find_all_by_person_id(1, :conditions => {:date => @week..@week.advance(:days => 6)}, 
-                                             :order => 'date DESC')
-    render :layout => 'print'
-  end
-
-  def allMonthlyReports
-    @events = Event.find(:all, :conditions => "category != 'Attempt' and 
-                                               category != 'Other' and
-                                               category != 'MoveIn' and
-                                               category != 'MoveOut' and
-                                               category is not NULL", 
-                         :order => 'date DESC')
-    @event_months = @events.group_by { |month| month.date.at_beginning_of_month }
-    render :monthlyReport
-  end
-  
   def monthlyReport
     range =  2.months.ago.at_beginning_of_month.to_date
     @events = Event.find(:all, :conditions => ["(category != 'Attempt' and 
