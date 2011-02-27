dc959e13afdbe8de23d90719fff29086433b523d
Ubuntu 10.10 <ryan@ubuntu.(none)>
2011-02-27 10:44:44 -0800

    Made a version of the events that's table based.   Because if IE issues I'm not using it buy would like to revisit it later.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,29 @@ class Event < ActiveRecord::Base
   belongs_to :family;
   belongs_to :person;
   validates_presence_of :family_id
+  include TeachingRecordsHelper
 
+  def getCategory
+    if memberMilestones.include?([self.category,self.category])
+      "Milestone"
+    elsif self.category == "Attend Church"
+      "Attend<br>Church"
+    else
+      self.category
+    end
+  end
+
+  def getComment
+    wardRep = ""
+    if self.person
+      wardRep = self.person.full_name
+    end
+    if memberMilestones.include?([self.category,self.category])
+      "#{wardRep} &ndash #{self.category}.  #{self.comment}"
+    elsif self.comment and self.comment.size > 0
+      "#{wardRep} &ndash #{self.comment}" 
+    else
+      "#{wardRep}" 
+    end
+  end
 end
